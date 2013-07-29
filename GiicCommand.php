d8a4a5044eba968b4078ad4c8c37fbbd7f824db5
Tobias Munk <schmunk@usrbin.de>
2013-07-30 01:44:09 +0200

    added comments

--- GiicCommand.php
+++ GiicCommand.php
@@ -34,21 +34,31 @@ EOD;
      */
     public function actionGenerate($args)
     {
+        // fake input params
         $_SERVER['REQUEST_URI']     = "console://index.php";
         $_SERVER['SCRIPT_FILENAME'] = $_SERVER['SCRIPT_NAME'] = "index.php";
         $_POST['generate']          = true;
         $_POST['answers']           = true;
+
+        // create gii module for controller
         Yii::import('system.gii.*');
         $module           = Yii::createComponent('system.gii.GiiModule', 'gii', null);
         $module->password = false;
 
+        // TODO: temporary confirm for overwriting files
         define('GIIC_ALL_CONFIRMED', true);
+
+        // load config
         $config = require(Yii::getPathOfAlias($args[0]) . "/giic-config.php");
         #var_dump($config);exit;
 
+        // execute actions (run gii controller action multiple times)
         foreach ($config['actions'] AS $action) {
+
+            // fake input param
             $_POST[$action['template'] . "Code"] = $action['model'];
 
+            // TODO: create controller and assign templates
             if ($action['template'] == "FullCrud") {
                 $controller            = Yii::createComponent(
                     'vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator',
@@ -76,23 +86,26 @@ EOD;
                 );
             }
 
-            $module->layout        = null;
-            Yii::app()->controller = $controller;
-
+            // message
             echo $action['template'].' - '.substr(implode(', ',$action['model']),0,80);
             echo "\n\n";
 
+            // assign controller to application
+            Yii::app()->controller = $controller;
+
+            // capture output from controller
             ob_start();
             $controller->run('index');
             $html = ob_get_clean();
+
+            // sanitize
             $html = str_replace("&nbsp;","",$html); // XSLT hotfix
 
+            // parse for console output
             $xslt = new XSLTProcessor();
             $xslt->importStylesheet(new SimpleXMLElement(file_get_contents(dirname(__FILE__).'/giic.xsl')));
             file_put_contents(dirname(__FILE__).'/giic.html', $html);
             echo $xslt->transformToXml(new SimpleXMLElement($html));
-
-            //exit;
         }
     }
 }
