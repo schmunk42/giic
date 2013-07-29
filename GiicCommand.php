3e491bbe46886b3a525d910cf3f360ec6cdf358b
Tobias Munk <schmunk@usrbin.de>
2013-07-29 18:58:45 +0200

    added config parsing

--- GiicCommand.php
+++ GiicCommand.php
@@ -33,30 +33,35 @@ EOD;
      * @param array $args command line parameters specific for this command
      */
     public function actionGenerate($args)
-    {
-        Yii::import('vendor.phundament.p3extensions.behaviors.*');
-
+    {               
         $_SERVER['REQUEST_URI'] = "console://index.php";
         $_SERVER['SCRIPT_FILENAME'] = $_SERVER['SCRIPT_NAME'] = "index.php";
         $_POST['generate'] = true;
         $_POST['answers'] = true;
-        define('GIIC_ALL_CONFIRMED', true);
-
-        $_POST['FullCrudCode'] = array(
-            'model'=>'vendor.phundament.p3pages.models.P3Page',
-            'controller' => 'test/p3Page'
-        );
-
         Yii::import('system.gii.*');
         $module = Yii::createComponent('system.gii.GiiModule', 'gii', null);
         $module->password = false;
-        $controller = Yii::createComponent('vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator', 'fullCrud', $module);
 
-        Yii::app()->controller = $controller;
-        $controller->templates = array('slim'=>realpath(dirname(__FILE__).'/../../phundament/gii-template-collection/fullCrud/templates/slim'));
+        define('GIIC_ALL_CONFIRMED', true);
+        $config = require(Yii::getPathOfAlias($args[0])."/giic-config.php");        
+        #var_dump($config);exit;
+        
+        foreach($config['actions'] AS $action) {
+             $_POST[$action['template']."Code"] = $action['model'];
+             
+             if ($action['template'] == "FullCrud")  {
+                 $controller = Yii::createComponent('vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator', 'fullCrud', $module);
+                 $controller->templates = array('slim'=>realpath(dirname(__FILE__).'/../../phundament/gii-template-collection/fullCrud/templates/slim'));
+                 }
+             else {
+                 $controller = Yii::createComponent('vendor.phundament.gii-template-collection.fullModel.FullModelGenerator', 'fullModel', $module);
+             $controller->templates = array('default'=>realpath(dirname(__FILE__).'/../../phundament/gii-template-collection/fullModel/templates/default'));
+        }
 
-        $controller->run('index');
-
-
+             $module->layout = null;
+             Yii::app()->controller = $controller;
+             $controller->run('index');
+             //exit;
+        }
     }
 }
